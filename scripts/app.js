21825dbab2758602b23b0915d2a0ab362b5196c5
Sanskriti <sanskritisri27@gmail.com>
2024-12-31 12:32:52 +0530

    tried to confirgure firebase and lowkey ended up in tears

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+import { app } from "./firebaseConfig.js";
+
+const form = document.getElementById("sign-up")
+const email = form.email.value
+const password = form.password.value
+
---
+++ scripts/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfEKReRyXnPP2bPIOs6fZzqpGSAs8gnoQ",
+  authDomain: "fir-learning-7944f.firebaseapp.com",
+  projectId: "fir-learning-7944f",
+  storageBucket: "fir-learning-7944f.firebasestorage.app",
+  messagingSenderId: "189930966808",
+  appId: "1:189930966808:web:eb1da3a53630e419d7186d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
